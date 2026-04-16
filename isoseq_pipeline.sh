a01c58714241e4fe29391abccb3d940dc5803fd9
ronan-m-doyle <ronan.doyle@synnovis.co.uk>
2026-04-16 15:51:55 +0100

    modified:   isoseq_pipeline.sh

--- isoseq_pipeline.sh
+++ isoseq_pipeline.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # ----------------------------------------------------------------------
-# Isolate Sequencing Master Pipeline v0.6
+# Isolate Sequencing Master Pipeline v0.7
 # ----------------------------------------------------------------------
 IFS=$'\n\t'
 
@@ -41,8 +41,13 @@ fi
 RUN_NAME=$(basename "$RUN_DIR")
 mkdir -p "pipeline_logs"
 RUN_TIMESTAMP=$(date '+%Y%m%d_%H%M%S')
+<<<<<<< HEAD
 touch "pipeline_logs/pipeline_summary_${RUN_NAME}_${RUN_TIMESTAMP}.tsv"
 MASTER_SUMMARY="pipeline_logs/pipeline_summary_${RUN_NAME}_${RUN_TIMESTAMP}.tsv"
+=======
+touch "pipeline_logs/pipeline_summary_${RUN_NAME}_${RUN_TIMESTAMP}.txt"
+MASTER_SUMMARY="pipeline_logs/pipeline_summary_${RUN_NAME}_${RUN_TIMESTAMP}.txt"
+>>>>>>> 5697c77 (	modified:   isoseq_pipeline.sh)
 echo -e "Sample\tStatus\tFailed_Step\tTotal_Time(s)" > "$MASTER_SUMMARY"
 
 echo "Monitoring sequencing run..."
@@ -52,7 +57,10 @@ STAGE1_FLAG="pipeline_logs/.8h_complete_${RUN_NAME}"
 STAGE2_FLAG="pipeline_logs/.24h_complete_${RUN_NAME}"
 STAGE3_FLAG="pipeline_logs/.48h_complete_${RUN_NAME}"
 STAGE4_FLAG="pipeline_logs/.72h_complete_${RUN_NAME}"
+<<<<<<< HEAD
 TREE_FLAG="pipeline_logs/.tree_complete_${RUN_NAME}"
+=======
+>>>>>>> 5697c77 (	modified:   isoseq_pipeline.sh)
 
 while true; do
 
@@ -68,9 +76,7 @@ while true; do
     # ---------------------------------
     # Calculate active sequencing time
     # ---------------------------------
-    last_start=$(awk -F'\t' 'END {print $11}' "$SUMMARY_FILE")
-
-    last_start=${last_start%.*}
+    last_start=$(awk -F'\t' 'NR>1 {if ($11>max) max=$11} END {print int(max)}' "$SUMMARY_FILE")
 
     elapsed_hours=$((last_start / 3600))
 
@@ -85,18 +91,18 @@ while true; do
         touch "$STAGE1_FLAG"
     fi
 
-    # ---------------------------------
-    # 24h Stage (≥24 hours)
-    # ---------------------------------
+     #---------------------------------
+     #24h Stage (≥24 hours)
+     #---------------------------------
     if (( last_start >= 86400 )) && [[ ! -f "$STAGE2_FLAG" ]]; then
         echo "▶ Running 24h analysis"
         bash isoseq_pipeline_24h.sh "$SAMPLESHEET" "$THREADS" "$RUN_DIR" "$MASTER_SUMMARY" "$RUN_NAME"
         touch "$STAGE2_FLAG"
     fi
 
-    # ---------------------------------
-    # 48h Stage (≥48 hours)
-    # ---------------------------------
+     #---------------------------------
+     #48h Stage (≥48 hours)
+     #---------------------------------
     if (( last_start >= 172800 )) && [[ ! -f "$STAGE3_FLAG" ]]; then
         echo "▶ Running 48h analysis"
         bash isoseq_pipeline_48h.sh "$SAMPLESHEET" "$THREADS" "$RUN_DIR" "$MASTER_SUMMARY" "$RUN_NAME"
@@ -104,12 +110,37 @@ while true; do
     fi
 
     # ---------------------------------
-    # 72h Stage (≥72 hours)
+    # Detect End of Run (Final summary file)
     # ---------------------------------
+<<<<<<< HEAD
     if (( last_start >= 258000 )) && [[ ! -f "$STAGE4_FLAG" ]]; then
         echo "▶ Running 72h analysis"
         bash isoseq_pipeline_72h.sh "$SAMPLESHEET" "$THREADS" "$RUN_DIR" "$MASTER_SUMMARY" "$RUN_NAME"
         touch "$STAGE4_FLAG"
+=======
+    FINAL_SUMMARY_FILE=$(find "$RUN_DIR" -name "final_summary_*.txt" | head -n 1)
+
+    if [[ -n "$FINAL_SUMMARY_FILE" ]]; then
+
+        echo "[$(date)] End-of-run detected"
+        echo "Final summary file: $FINAL_SUMMARY_FILE"
+
+   	# ---------------------------------
+  	# 72h Stage (End of run)
+	# ---------------------------------
+        if [[ ! -f "$STAGE4_FLAG" ]]; then
+        	echo "▶ Running 72h analysis"
+        	bash isoseq_pipeline_72h.sh "$SAMPLESHEET" "$THREADS" "$RUN_DIR" "$MASTER_SUMMARY" "$RUN_NAME"
+        	touch "$STAGE4_FLAG"
+    	fi
+    
+    	# ---------------------------------
+    	# Tree building Stage - Will always re-run if pipeline is started
+    	# ---------------------------------
+        echo "▶ Running phylogenetic analysis"
+        bash isoseq_pipeline_tree.sh "$SAMPLESHEET" "$THREADS" "$RUN_DIR" "$MASTER_SUMMARY" "$RUN_NAME"
+
+>>>>>>> 5697c77 (	modified:   isoseq_pipeline.sh)
         echo "All stages complete. Exiting monitor."
         break
     fi
@@ -124,6 +155,7 @@ while true; do
         echo "All stages complete. Exiting monitor."
         break
     fi
+
 
     # ---------------------------------
     # Sleep 30 minutes
